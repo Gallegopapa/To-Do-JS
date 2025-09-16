ebcef8485f052ceb031790b69ae732ac6afe8a91
Eileen <eileenlc0509@gmail.com>
2025-09-16 07:26:32 -0500

    redireccionar a inicio luego de eliminar tarea

--- eliminar.php
+++ eliminar.php
@@ -2,8 +2,9 @@
 include("config.php");
 
 $id = $_GET['id'];
+
 $conn->query("DELETE FROM tasks WHERE parent_task_id=$id");
 $conn->query("DELETE FROM tasks WHERE id=$id");
 
-header("Location: tareas.php");
+header("Location: inicio.php");
 exit();
